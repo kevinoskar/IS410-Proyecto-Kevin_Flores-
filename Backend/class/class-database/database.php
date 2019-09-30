de3c0f915a698c6d12166e287e8ce259b60fa0e7
kevinoskar <kevin.oskar7@gmail.com>
2019-09-29 21:31:21 -0600

    Integrando la clase Database

---
+++ Backend/class/class-database/database.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('../../vendor/autoload.php');
+    use Kreait\Firebase\Factory;
+
+    class Database{
+        private $keyFile = '../../key/fly-database-93a13-9fb6fdab50f1.json';
+        private $URI = 'https://fly-database-93a13.firebaseio.com/';
+        private $db;
+        public function __construct(){
+            $firebase = (new Factory)
+                ->withServiceAccount($this->keyFile)
+                ->withDatabaseUri($this->URI)
+                ->create();
+            $this->db = $firebase->getDatabase();
+        }
+        public function getDB(){
+            return $this->db;
+        }
+    }
+?>
