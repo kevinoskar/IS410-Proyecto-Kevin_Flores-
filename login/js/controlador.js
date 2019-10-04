7136a6fa15dc4856fe281231021e6c728b890f88
kevinoskar <kevin.oskar7@gmail.com>
2019-10-04 13:07:51 -0600

    Login Backend

--- login/js/controlador.js
+++ login/js/controlador.js
@@ -1,4 +1,15 @@
 
+$('#spinner').hide();
+let campos=[
+    {id:'emailSuperuser',campoValido:false},
+    {id:'passwordSuperuser',campoValido:false}
+];
+$('.dropdown-menu').on('click', function (e) {
+  e.stopPropagation();
+});
+
+
+
 function redirect(){
   window.location="../registrar-usuario/index.html";
 }
@@ -14,4 +25,60 @@ function redirect4(){
 function redirectHome(){
  console.log("Acceder al Home")
   window.location="../Pagina-Central/index.html"
+}
+
+function iniciarSesionSU(){
+  $('#spinner').show();
+    console.log(`Validar Campos: Usuario: `+ document.getElementById("emailSuperuser").value +` Password: `+ document.getElementById("passwordSuperuser").value);
+    for(var i=0;i<campos.length;i++)
+        validarCampoVacio(campos[i].id,i);
+    
+    campos[0].campoValido=validarEmail(document.getElementById('emailSuperuser').value);
+    if(campos[0].campoValido==true && campos[1].campoValido==true){
+      let parametros=$('#formSuperuser').serialize();
+      console.log(parametros);
+      $.ajax({
+        url:'../Backend/ajax/ajax-superuser/superuser.php?action=login',
+        method:'POST',
+        dataType:'json',
+        data:parametros,
+        success:(res)=>{
+            console.log(res);
+            $('#spinner').hide();
+            window.location.href="../../Proyecto-IIPAC/superusuario/superusuario.html"
+        },
+        error:(error)=>{
+            console.log(error);
+        }
+      });
+    }
+}
+function validarCampoVacio(id,i){
+  if(document.getElementById(id).value==''){
+      campos[i].campoValido=false;
+      document.getElementById(id).classList.remove("is-valid");
+      document.getElementById(id).classList.add("is-invalid");
+      return;
+  }else{
+      campos[i].campoValido=true;
+      document.getElementById(id).classList.remove("is-invalid");
+      document.getElementById(id).classList.add("is-valid");
+  }
+}
+function validarCampoEmail(id,valido){
+  if(valido){
+      document.getElementById(id).classList.remove("is-invalid");
+      document.getElementById(id).classList.add("is-valid");
+  }else{
+      document.getElementById(id).classList.remove("is-valid");
+      document.getElementById(id).classList.add("is-invalid");
+  }
+}
+
+function validarEmail(email){
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let resultado =  re.test(email);
+  console.log(`El email es correcto: `+resultado);
+  validarCampoEmail('emailSuperuser',resultado);
+  return resultado;
 }
